0c009799e925d2eb152c081a303cca89610781aa
fuzznimp <montaigne.anthony.dev@gmail.com>
2019-06-12 16:30:34 +0200

    added linkedin to about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,25 +3,39 @@ import styled from 'styled-components';
 
 const About = () => {
   return (
-    <div>
-      <h2>Hello</h2>
+    <AboutContainer>
+      <h2>Developed by Anthony Montaigne</h2>
       <GithubLink>
-        <a href="https://github.com/Fuzznimp/spacex_web_datacenter" target="_blank">
-          <i className="fab fa-github" />
+        <a
+          href="https://github.com/Fuzznimp/spacex_web_datacenter"
+          target="_blank">
+          <i className="fab fa-github-square" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anthony-montaigne-abb210176/"
+          target="_blank">
+          <i className="fab fa-linkedin" />
         </a>
       </GithubLink>
-    </div>
+    </AboutContainer>
   );
 };
 
 export default About;
 
 // Style
+const AboutContainer = styled.div`
+  margin: 200px 0;
+`;
+
 const GithubLink = styled.div`
-  font-size: 100px;
+  display: flex;
+  justify-content: center;
+  font-size: 80px;
   a {
     text-decoration: none;
     color: white;
+    margin: 0 30px;
     transition: 0.3s;
     &:hover {
       color: #007bff;
