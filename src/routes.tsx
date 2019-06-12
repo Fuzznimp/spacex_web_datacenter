d4ed187784797a7a90cfe9f80804ba52d8c72643
fuzznimp <montaigne.anthony.dev@gmail.com>
2019-06-12 21:28:02 +0200

    Added conditional navbar rendering

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Router, Route } from 'react-router-dom';
 
 import RoutesHistory from './utils/history';
@@ -12,23 +12,50 @@ import Rockets from './pages/rockets/index';
 import History from './pages/history/index';
 import About from './pages/about/index';
 
-const Routes = () => {
-  return (
-    <div>
-      <Router history={RoutesHistory}>
-        <div>
-          <Header />
-          <HeaderMobile />
-          <Route path="/" exact component={Homepage} />
-          <Route path="/future" component={Future} />
-          <Route path="/past" component={Past} />
-          <Route path="/rockets" component={Rockets} />
-          <Route path="/history" component={History} />
-          <Route path="/about" component={About} />
-        </div>
-      </Router>
-    </div>
-  );
-};
+interface Props {}
+interface State {
+  isMobile: boolean;
+}
+
+class Routes extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false //This is where I am having problems
+    };
+
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener('resize', this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isMobile: window.innerWidth < 767 });
+  }
+  render() {
+    const isMobile = this.state.isMobile;
+    return (
+      <div>
+        <Router history={RoutesHistory}>
+          <div>
+            {isMobile ? <HeaderMobile /> : <Header />}
+            <Route path="/" exact component={Homepage} />
+            <Route path="/future" component={Future} />
+            <Route path="/past" component={Past} />
+            <Route path="/rockets" component={Rockets} />
+            <Route path="/history" component={History} />
+            <Route path="/about" component={About} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default Routes;
