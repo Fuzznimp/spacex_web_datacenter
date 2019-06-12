41fae07225fcef3dfa37600bf7770bc32469489d
fuzznimp <montaigne.anthony.dev@gmail.com>
2019-06-12 10:51:32 +0200

    Added github link

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const About = () => {
   return (
     <div>
       <h2>Hello</h2>
+      <GithubLink>
+        <a href="https://github.com/Fuzznimp/spacex_web_datacenter">
+          <i className="fab fa-github" />
+        </a>
+      </GithubLink>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
+
+// Style
+
+const GithubLink = styled.div`
+  font-size: 100px;
+  a {
+    text-decoration: none;
+    color: white;
+    transition: 0.3s;
+    &:hover {
+      color: #007bff;
+    }
+  }
+`;
