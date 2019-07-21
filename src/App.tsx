4c8942dbea5961c85b240bd26cb873a7d15d1b2d
fuzznimp <montaigne.anthony.dev@gmail.com>
2019-07-21 23:24:27 +0100

    feature(frontend): added console.log ascii

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Particles from 'react-particles-js';
 import client from './graphql/apollo/default';
 import { Routes } from './routes';
 import particlesParams from './lib/particlesParams';
+import { consoleLogAscii } from './utils/consoleLogAscii';
 
 import './App.css';
 
 const App = () => {
+  consoleLogAscii();
   return (
     <div className="App">
       <ApolloProvider client={client}>
